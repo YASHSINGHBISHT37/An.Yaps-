9246c7a02240dec9545ac6445c99700963e79cc0
YASH SINGH BISHT <154204394+YASHSINGHBISHT37@users.noreply.github.com>
2025-10-20 03:59:23 +0530

    Update Notify.jsx

--- src/pages/Notify.jsx
+++ src/pages/Notify.jsx
@@ -26,12 +26,12 @@ const Notify = ({ onClose }) => {
             {/* <div className='fixed bottom-0 w-full h-104 bg-[#161616] backdrop-blur-[1vh] rounded-t-3xl p-4 border-white/30'>
                 <h1 className='text-6xl tracking-tighter mb-2 text-white/80 font-bold'>Notifications</h1>
 
-                <div className="Notifications w-full h-60 border border-white/5 bg-[#161616]/0 rounded-4xl flex justify-center items-center p-4">
+                <div className="Notifications w-full h-60 border border-white/5 bg-[# rounded-4xl flex justify-center items-center p-4">
                     <h1 className='opacity-20 tracking-tight font-bold'>No Notifications</h1>
                 </div>
             </div> */}
 
-            <div className={`Baclground-Overlay absolute w-screen h-screen bg-[#161616]/0 top-0 opacity transition-opacity duration-600 ${visible ? 'opacity-100' : 'opacity-0'}`} onClick={handleClose}></div>
+            <div className={`Baclground-Overlay absolute w-screen h-screen bg-transparent top-0 opacity transition-opacity duration-600 ${visible ? 'opacity-100' : 'opacity-0'}`} onClick={handleClose}></div>
 
             <div className={`fixed w-full h-104 bg-[white]/90 backdrop-blur-[2vh] rounded-t-3xl p-4 border-t border-[#161616]/70 transform transition-transform duration-800
             ${closing ? 'bottom-0' : visible ? 'bottom-0' : 'bottom-[-60vh]'}
