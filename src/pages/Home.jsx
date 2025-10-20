478a542fd5da52aaca0e30d64b312857072b7c69
yash singh bisht <yashbisht0007@gmail.com>
2025-10-21 03:08:14 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,8 @@ import Stock from '../components/Home/Stock'
 import TopNav from '../components/Navigation/TopNav'
 
 const Home = () => {
-    // const [showNotify, setShowNotify] = useState(false)
-    // const [showFavourites, setShowFavourites] = useState(false)
-
     return (
         <div className='w-screen h-screen z-[999997] bg- select-none'>
-
-            {/* <h1 className='text-[#161616] relative text-9xl z-50 tracking-tighter font-bold'>Home</h1> */}
 
             {/* <AboutApp /> */}
             {/* <Login/> */}
